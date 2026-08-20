6cf7e687d20ca35b696198a7dc1a3dabe0745a98
Goutham-Sheshi <vvrk9d6mmt@privaterelay.appleid.com>
2026-08-20 15:37:04 +0530

    Wire admin event management into application

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import AuthGate from './AuthGate'
+import { startAdminBridge } from './adminBridge'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -11,3 +12,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </AuthGate>
   </React.StrictMode>,
 )
+
+startAdminBridge()
