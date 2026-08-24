44d399a65b91c1e0fb682ec4332fd17f7bd7fc24
Goutham-Sheshi <vvrk9d6mmt@privaterelay.appleid.com>
2026-08-24 11:32:25 +0530

    Use live data application and remove stale page bridges

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import LiveApp from './LiveApp'
 import AuthGate from './AuthGate'
 import { startAdminBridge } from './adminBridge'
 import { startAdminLabelBridge } from './adminLabelBridge'
-import { startEventViewsBridge } from './eventViewsBridge'
 import { startFileViewerBridge } from './fileViewerBridge'
-import { startFigmaPageBridge } from './figmaPageBridge'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthGate>
-      <App />
+      <LiveApp />
     </AuthGate>
   </React.StrictMode>,
 )
 
 startAdminBridge()
 startAdminLabelBridge()
-startEventViewsBridge()
 startFileViewerBridge()
-startFigmaPageBridge()
