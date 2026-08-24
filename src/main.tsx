94eee548ec4b4d188cd6a0c6f56f7fc5c4654503
Goutham-Sheshi <vvrk9d6mmt@privaterelay.appleid.com>
2026-08-24 16:36:04 +0530

    Remove brittle admin DOM bridge

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import LiveApp from './LiveApp'
 import AuthGate from './AuthGate'
-import { startAdminBridge } from './adminBridge'
-import { startAdminLabelBridge } from './adminLabelBridge'
 import { startFileViewerBridge } from './fileViewerBridge'
 import './index.css'
 
@@ -15,6 +13,4 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   </React.StrictMode>,
 )
 
-startAdminBridge()
-startAdminLabelBridge()
 startFileViewerBridge()
