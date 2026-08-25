21042609aeda80d1c91008c8f36459611c28aa7f
Goutham-Sheshi <vvrk9d6mmt@privaterelay.appleid.com>
2026-08-25 15:54:14 +0530

    Remove Vault authentication gate and restore direct app access

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import LiveApp from './LiveApp'
-import AuthGate from './AuthGate'
 import { startFileViewerBridge } from './fileViewerBridge'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthGate>
-      <LiveApp />
-    </AuthGate>
+    <LiveApp />
   </React.StrictMode>,
 )
 
